78646611aba051e952172e6d2bb166aa105738d1
Andrey Alexandrovich <x0rb0t@gmail.com>
2024-03-30 01:39:26 +0000

    Update server.py

--- server.py
+++ server.py
@@ -3,40 +3,20 @@ from flask import Flask, request, jsonify
 from diffusers import UNet2DConditionModel, StableDiffusionPipeline, StableDiffusionXLPipeline, DiffusionPipeline, LCMScheduler, EulerDiscreteScheduler, EulerAncestralDiscreteScheduler, AutoencoderKL
 import torch
 from PIL import Image, ImageOps
-
 import io
 import base64
+import logging
 
-# Function to parse command-line arguments
-def parse_args():
-    import argparse
-    parser = argparse.ArgumentParser(description='Stable Diffusion Server')
-    parser.add_argument('--model', type=str, default='stabilityai/stable-diffusion-xl-base-1.0', help='Model name for DiffusionPipeline')
-    parser.add_argument('--unet', type=str, default='', help='UNet model name')
-    parser.add_argument('--lora_dirs', type=str, default='', help='Colon-separated list of LoRA directories')
-    parser.add_argument('--lora_scales', type=str, default='', help='Colon-separated list of LoRA scales')
-    parser.add_argument('--scheduler', type=str, default='euler_a', help='Scheduler')
-    return parser.parse_args()
+from utils import parse_args, Args, is_local_file
 
-# Simple class to mimic argparse.Namespace behavior
-class Args:
-    def __init__(self, model, unet, lora_dirs, lora_scales, scheduler):
-        self.model = model
-        self.unet = unet
-        self.lora_dirs = lora_dirs
-        self.lora_scales = lora_scales
-        self.scheduler = scheduler
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-# Check if running under Gunicorn
 is_gunicorn = "gunicorn" in os.environ.get("SERVER_SOFTWARE", "")
 
-def is_local_file(path):
-    return os.path.isfile(path)
-    
 if not is_gunicorn:
     args = parse_args()
 else:
-    # Create Args from environment variables
     args = Args(
         model=os.getenv('MODEL_NAME', 'stabilityai/stable-diffusion-xl-base-1.0'),
         unet=os.getenv('UNET_MODEL', ''),
@@ -47,85 +27,92 @@ else:
 
 app = Flask(__name__)
 vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16)
-# Load the models
-if args.unet == '':
-    if is_local_file(args.model):
-        pipe = StableDiffusionXLPipeline.from_single_file(args.model, vae = vae, torch_dtype=torch.float16, variant="fp16", use_safetensors=True)
-    else:    
-        pipe = DiffusionPipeline.from_pretrained(args.model, vae = vae, torch_dtype=torch.float16, variant="fp16")
-else:
-    unet = UNet2DConditionModel.from_pretrained(args.unet, torch_dtype=torch.float16, variant="fp16")
-    if is_local_file(args.model):
-        pipe = StableDiffusionXLPipeline.from_single_file(args.model, vae = vae, unet=unet, torch_dtype=torch.float16, variant="fp16", use_safetensors=True)
+
+def load_models():
+    if args.unet == '':
+        if is_local_file(args.model):
+            pipe = StableDiffusionXLPipeline.from_single_file(args.model, vae=vae, torch_dtype=torch.float16, variant="fp16", use_safetensors=True)
+        else:    
+            pipe = DiffusionPipeline.from_pretrained(args.model, vae=vae, torch_dtype=torch.float16, variant="fp16")
     else:
-        pipe = DiffusionPipeline.from_pretrained(args.model, vae = vae, unet=unet, torch_dtype=torch.float16, variant="fp16")
+        unet = UNet2DConditionModel.from_pretrained(args.unet, torch_dtype=torch.float16, variant="fp16")
+        if is_local_file(args.model):
+            pipe = StableDiffusionXLPipeline.from_single_file(args.model, vae=vae, unet=unet, torch_dtype=torch.float16, variant="fp16", use_safetensors=True)
+        else:
+            pipe = DiffusionPipeline.from_pretrained(args.model, vae=vae, unet=unet, torch_dtype=torch.float16, variant="fp16")
 
-# Process and load LoRA weights and scales
-lora_dirs = args.lora_dirs.split(':') if args.lora_dirs else []
-lora_scales = [float(scale) for scale in args.lora_scales.split(':')] if args.lora_scales else []
+    lora_dirs = args.lora_dirs.split(':') if args.lora_dirs else []
+    lora_scales = [float(scale) for scale in args.lora_scales.split(':')] if args.lora_scales else []
 
-if len(lora_dirs) != len(lora_scales):
-    raise ValueError("The number of LoRA directories must match the number of scales")
+    if len(lora_dirs) != len(lora_scales):
+        raise ValueError("The number of LoRA directories must match the number of scales")
 
-for ldir, lsc in zip(lora_dirs, lora_scales):
-    pipe.load_lora_weights(ldir)
-    pipe.fuse_lora(lora_scale=lsc)
+    for ldir, lsc in zip(lora_dirs, lora_scales):
+        pipe.load_lora_weights(ldir)
+        pipe.fuse_lora(lora_scale=lsc)
 
-if args.scheduler == "lcm":
-    pipe.scheduler = LCMScheduler.from_config(pipe.scheduler.config)
-elif args.scheduler == "euler":
-    pipe.scheduler = EulerDiscreteScheduler.from_config(pipe.scheduler.config)
-elif args.scheduler == "euler_a":
-    pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)
+    if args.scheduler == "lcm":
+        pipe.scheduler = LCMScheduler.from_config(pipe.scheduler.config)
+    elif args.scheduler == "euler":
+        pipe.scheduler = EulerDiscreteScheduler.from_config(pipe.scheduler.config)
+    elif args.scheduler == "euler_a":
+        pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)
 
-pipe.to("cuda")
+    pipe.to("cuda")
 
+    # Compile the UNet and VAE
+    pipe.unet = torch.compile(pipe.unet, mode="max-autotune", fullgraph=True)
+    pipe.vae.decode = torch.compile(pipe.vae.decode, mode="max-autotune", fullgraph=True)
+
+    return pipe
+
+pipe = load_models()
 
 @app.route('/generate-image', methods=['POST'])
 def generate_image():
-    data = request.json
-    prompt = data.get("prompt")
-    negative_prompt = data.get("negative_prompt", None)  # New negative prompt parameter
-    num_inference_steps = data.get("num_inference_steps", 30)
-    guidance_scale = data.get("guidance_scale", 7.5)
-    seed = data.get("seed", None)  # Retrieve seed from request; default is None
-    image_format = data.get("format", "jpeg").lower() # Default format is jpeg
+    try:
+        data = request.json
+        prompt = data.get("prompt")
+        negative_prompt = data.get("negative_prompt", None)
+        num_inference_steps = data.get("num_inference_steps", 30)
+        guidance_scale = data.get("guidance_scale", 7.5)
+        seed = data.get("seed", None)
+        image_format = data.get("format", "jpeg").lower()
 
-    original_width = data.get("width", 1024)
-    original_height = data.get("height", 1024)
-    
-    # Adjust width and height to the nearest multiple of 8
-    width = ((original_width + 7) // 8) * 8
-    height = ((original_height + 7) // 8) * 8
-    
-    if seed is not None:
-        generator = torch.manual_seed(seed)
-    else:
-        generator = None
-        
-    # Validation for image format
-    if image_format not in ["jpeg", "png"]:
-        return jsonify({"error": "Invalid image format. Choose 'jpeg' or 'png'."}), 400
+        original_width = data.get("width", 1024)
+        original_height = data.get("height", 1024)
 
-    # Image generation
-    image = pipe(prompt, negative_prompt=negative_prompt, width=width, height=height, num_inference_steps=num_inference_steps, guidance_scale=guidance_scale).images[0]
+        width = ((original_width + 7) // 8) * 8
+        height = ((original_height + 7) // 8) * 8
 
-    if (width != original_width) or (height != original_height):
-        # Calculate cropping dimensions
-        left = (width - original_width) // 2
-        top = (height - original_height) // 2
-        right = left + original_width
-        bottom = top + original_height
+        if seed is not None:
+            generator = torch.manual_seed(seed)
+        else:
+            generator = None
 
-        image = image.crop((left, top, right, bottom))
+        if image_format not in ["jpeg", "png"]:
+            return jsonify({"error": "Invalid image format. Choose 'jpeg' or 'png'."}), 400
 
-    # Convert image to Data URI
-    buffer = io.BytesIO()
-    image.save(buffer, format=image_format)
-    mime_type = "image/jpeg" if image_format == "jpeg" else "image/png"
-    data_uri = "data:" + mime_type + ";base64," + base64.b64encode(buffer.getvalue()).decode('utf-8')
+        image = pipe(prompt, negative_prompt=negative_prompt, width=width, height=height, num_inference_steps=num_inference_steps, guidance_scale=guidance_scale).images[0]
 
-    return jsonify({"image": data_uri})
+        if (width != original_width) or (height != original_height):
+            left = (width - original_width) // 2
+            top = (height - original_height) // 2
+            right = left + original_width
+            bottom = top + original_height
+
+            image = image.crop((left, top, right, bottom))
+
+        buffer = io.BytesIO()
+        image.save(buffer, format=image_format)
+        mime_type = "image/jpeg" if image_format == "jpeg" else "image/png"
+        data_uri = "data:" + mime_type + ";base64," + base64.b64encode(buffer.getvalue()).decode('utf-8')
+
+        return jsonify({"image": data_uri})
+
+    except Exception as e:
+        logger.exception("Error generating image")
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(port=3101)
